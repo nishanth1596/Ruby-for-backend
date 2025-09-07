d5355798afd2128a1544ba23c95a0a52fa869624
Nishanth <nishanth1596@gmail.com>
2025-09-07 10:52:38 +0530

    first commit

---
+++ playground.rb
@@ -0,0 +1,65 @@
+# # puts "hello world"
+
+# # greeting = 'Hello world!@#'
+# # puts greeting
+
+# # def say_hello(heya)
+# #     puts heya
+# # end
+
+# # say_hello("hello nishanth")
+
+# # first_name= "Nishanth"
+# # last_name = 'Venkatesan'
+
+# # puts 'My first name is #{first_name} and last name is #{last_name}'
+# # puts "My first name is #{first_name} and last name is #{last_name}"
+# # puts first_name.method
+
+# # puts "What is your first name?"
+# # first_name = gets.chomp
+# # puts "Hello #{first_name}"
+
+# # puts "Enter a number to multiply by 2"
+# # input = gets.chomp
+# # puts input.to_i*2
+
+# # puts "Enter your first name"
+# # first_name= gets.chomp
+# # puts   "Enter your last name"
+# # last_name = gets.chomp
+# # puts "Your full name is #{first_name} #{last_name}"
+# # puts "Your full name reversed is #{last_name.reverse!} #{first_name.reverse!}"
+# # puts "Your name has #{first_name.length + last_name.length} characters in it"
+
+# # puts 1+2
+
+# # puts "Simple Calculator"
+# # 25.times{ print "-"}
+# # puts "Enter the first number"
+# # num_1 = gets.chomp.to_i
+# # puts "Enter the second number"
+# # num_2 = gets.chomp.to_i
+# # puts "The first number multiplied by the second number is #{num_1 * num_2}"
+
+# # def multiply(first_num, second_num)
+# #     first_num.to_i * second_num.to_i
+# # end
+# # puts multiply(3,7)
+
+# # if !true
+# #     puts "Hello"
+# # else
+# #     puts "Bye"
+# # end
+
+# a = [1,2,3,4,5,6,7,8,9]
+# # puts a
+# # print a
+# # p a
+# # p a[8]
+# p a.last
+
+simple_hash = {'a' => 1, 'b' => 2, 'c' => 3}
+print simple_hash['c']
+print simple_hash.keys
