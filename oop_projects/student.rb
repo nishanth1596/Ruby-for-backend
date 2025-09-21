992cf2ae354cead784cb9573a639d0e254e38164
Nishanth <nishanth1596@gmail.com>
2025-09-21 19:09:23 +0530

    add student class

---
+++ oop_projects/student.rb
@@ -0,0 +1,24 @@
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(first_name, last_name, username, email, password)
+    @first_name = first_name
+    @last_name = last_name
+    @username = username
+    @email = email
+    @password = password
+  end
+    
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, email address: #{@email}"
+  end
+end
+
+nishanth = Student.new("Nishanth", "Venkatesan", "nishnath14435", "nishanth1fswedf@gmail.com", "password1")
+john = Student.new("john", "V", "john1", "john1@gmail.com", "john1")
+
+puts nishanth
+puts john
+nishanth.last_name = john.last_name
+puts "Nishanth is altered"
+puts nishanth
